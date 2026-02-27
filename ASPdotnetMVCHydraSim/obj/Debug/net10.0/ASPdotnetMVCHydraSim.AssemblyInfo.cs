f01b1d457e5b2508c192355335463f079b13aed1
Raf <67225513+Raf-hm@users.noreply.github.com>
2026-02-27 15:13:32 +0100

    Small ui updates.

--- ASPdotnetMVCHydraSim/obj/Debug/net10.0/ASPdotnetMVCHydraSim.AssemblyInfo.cs
+++ ASPdotnetMVCHydraSim/obj/Debug/net10.0/ASPdotnetMVCHydraSim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPdotnetMVCHydraSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a57c5c7883896797d42a62f40cce5f7cd75570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a075a1dba3093f2874a65fb6282ed7d0527c9b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPdotnetMVCHydraSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPdotnetMVCHydraSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
