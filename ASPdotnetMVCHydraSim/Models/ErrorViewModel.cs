85db6bd34eaf2a568c09a3fb900e9b195632da75
Raf <67225513+Raf-hm@users.noreply.github.com>
2026-04-28 23:07:23 +0200

    Oude projectmap verwijderd.

--- ASPdotnetMVCHydraSim/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPdotnetMVCHydraSim.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
